967f24e21ff168923192403ae2f08295ce9b542a
Ganeshk750 <ganesh9ot@gmail.com>
2021-12-06 13:14:01 +0530

    # fixed dropdwonUI

--- src/components/Filters/InputGroup.js
+++ src/components/Filters/InputGroup.js
@@ -1,23 +1,24 @@
 import React from 'react'
 
 const InputGroup = ({ total, name, setId }) => {
-    console.log();
     return (
-        <div className="input-group mb-3">  
-            <select 
-            onChange={e => setId(e.target.value)}
-            className="from-select" id={name}>
-            <option selected>Choose...</option>  
-            {
-                [...Array(total).keys()].map((ele, index) =>{
-                   return <option value={ele + 1}>
-                   {name} - {ele + 1}
-                   </option>
-                })
-              } 
-            </select>
+        <div className="input-group mb-3">
+          <select
+            onChange={(e) => setId(e.target.value)}
+            className="form-select"
+            id={name}
+          >
+            <option value="1">Choose...</option>
+            {[...Array(total).keys()].map((ele, index) => {
+              return (
+                <option value={ele + 1}>
+                  {name} - {ele + 1}
+                </option>
+              );
+            })}
+          </select>
         </div>
-    )
+      );
 }
 
 export default InputGroup
